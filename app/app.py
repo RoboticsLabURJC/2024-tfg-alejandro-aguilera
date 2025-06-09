8eba0cc17af4042f0c9ce26cbe205e9f9d3bb35e
aaguilera13 <aa.aguileralopez@gmail.com>
2025-06-09 14:01:14 +0200

    cambios en la app

--- app/app.py
+++ app/app.py
@@ -9,10 +9,8 @@ import pages.page_3a as page_3a
 import pages.page_3b as page_3b
 import pages.page_3c as page_3c
 
-
 app = Flask(__name__)
 
-# Integrar los Dash apps con Flask
 app = page_1a.init_dashboard(app)
 app = page_1b.init_dashboard(app)
 app = page_1c.init_dashboard(app)
@@ -20,15 +18,18 @@ app = page_2a.init_dashboard(app)
 app = page_2b.init_dashboard(app)
 app = page_2c.init_dashboard(app)
 app = page_3a.init_dashboard(app)
-
 app = page_3b.init_dashboard(app)
 app = page_3c.init_dashboard(app)
 
-
-# Ruta del menú principal
+# Ruta del menú graficas
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", active_page="dashboard")
+
+# Ruta para la documentación
+@app.route("/documentacion")
+def documentacion():
+    return render_template("documentacion.html", active_page="documentacion")
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
